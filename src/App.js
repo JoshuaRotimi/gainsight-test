3802fcedbc7d98bd19703627c8c5ac81c4571cd4
JoshuaRotimi <62189959+JoshuaRotimi@users.noreply.github.com>
2022-08-03 00:21:49 +0100

    added api call

--- src/App.js
+++ src/App.js
@@ -1,65 +1,124 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useCallback} from "react";
 import './App.css';
-import {Box, Heading, Text, Flex, Input, Button} from '@chakra-ui/react';
+import {Box, Heading, Text, Flex, Input, Button, Link} from '@chakra-ui/react';
 import Item from "./components/Item/Item";
 import {Octokit} from "octokit";
 
 function App() {
     const [token, setToken] = useState('');
+    const [error, setError] = useState('');
     const [loading, setLoading] = useState(false);
     const [data, setData] = useState([]);
+    const [countDown, setCountDown] = useState(30);
 
-    const ACCESS_TOKEN = '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';
+
+    const connectKey = window.localStorage.getItem('accessKey');
+ //   console.log(connectKey, 'connector');
+
+
+    const ACCESS_TOKEN = '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';
+    const REPO_NAME = 'typing-test';
 
 
     const octokit = new Octokit({ auth: ACCESS_TOKEN });
 
     const getData = async () => {
         try {
+            setError('');
             setLoading(true);
-            const {data: { login }} = await octokit.rest.users.getAuthenticated();
-            console.log("Hello, %s", login);
+           const {data: { login }} = await octokit.rest.users.getAuthenticated();
+           console.log("Hello, %s", login);
 
             const info = await octokit.request('GET /repos/{owner}/{repo}/commits', {
                 owner: 'JoshuaRotimi',
-                repo: 'typing-test'
+                repo: REPO_NAME
             });
             console.log(info.data);
             setData(info.data);
             setLoading(false);
 
         } catch (e) {
-            console.log(e)
+            console.log(e);
+            setLoading(false);
+            setError('Something went wrong. Could not fetch data.')
         }
 
     };
 
     const handleChange = (e) => {
-        setToken(e.target.value);
+        if (e.target.value === '') {
+            return;
+        }
+        setToken(e.target.value.trim());
     };
+
+    const handleClick = ()  => {
+        const saveToken = window.localStorage.setItem('accessKey', token);
+
+    };
+
+    const handleRefresh = useCallback(() => {
+        getData();
+    }, []);
 
     useEffect(() => {
         getData();
     }, []);
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            console.log('Logs every minute');
+            setCountDown((prevState) => {
+                if (prevState === 0) {
+                    getData();
+                    setCountDown(30);
+                } else {
+                    return prevState - 1
+                }
+            });
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
   return (
       <Box p={'10px'} w={'50%'} m={'10px auto'} textAlign={'center'}>
           <Heading m={'10px 0'}>Gainsight Project</Heading>
+          <Text
+              p={'20px 0'}
+              fontSize={'18px'}
+          >Showing latest commits from this {' '}
+              <Link textDecoration={'underline'} href={`https://github.com/JoshuaRotimi/${REPO_NAME}`} isExternal> Repository</Link>
+          </Text>
 
-          <Text>Please enter the access token to view Latest Commits</Text>
-          <Flex w={'80%'} m={'20px auto'}>
-              <Input textAlign='left'
-                     fontSize='14px'
-                     placeholder='Enter Token'
-                     value={token} type={'text'}
-                     onChange={handleChange}
-              />
-              <Button colorScheme={'teal'} m={'0 10px'}>Submit</Button>
-          </Flex>
+          {
+              connectKey ? null : (
+                  <Box>
+                      <Text>Please enter the access token to view Latest Commits</Text>
+                      <Flex w={'80%'} m={'20px auto'}>
+                          <Input textAlign='left'
+                                 fontSize='14px'
+                                 placeholder='Enter Token'
+                                 value={token} type={'text'}
+                                 onChange={handleChange}
+                          />
+                          <Button colorScheme={'teal'} m={'0 10px'}>Submit</Button>
+                      </Flex>
+                  </Box>)
+          }
 
           <Flex alignItems={'center'} flexDirection={'column'}>
-              {data && data.map((item) => (
-                  <Item key={item.index} data={data}/>
+              <Flex alignItems={'center'}>
+                  <Button colorScheme={'teal'} m={'0 10px'} onClick={handleRefresh}>Refresh</Button>
+                  <Text color={'lightblue'}>{countDown}</Text>
+              </Flex>
+
+              {loading && <Text p={'20px 0'}>Loading... Please wait.</Text>}
+
+              {error && <Text p={'20px 0'}>{error}</Text>}
+
+              {data && data.map((item, index) => (
+                  <Item key={index} data={item}/>
               ))}
           </Flex>
 
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,7 +2,6 @@ import React from "react";
 import {Box, Text, Flex, Heading} from '@chakra-ui/react';
 
 const Item = ({data}) => {
-    console.log(data);
     return (
         <Box m={'20px 0'} border={'1px solid gray'} p={'5px'} borderRadius={'4px'} w={'60%'}>
             <Heading
@@ -11,10 +10,10 @@ const Item = ({data}) => {
                 as={'h4'}
                 size={'md'}
                 fontSize={'20px'}
-                m={'20px 0'}>Initial Commit</Heading>
+                m={'20px 0'}>{data.commit.message}</Heading>
             <Flex m={'5px 0'} justifyContent={'space-between'}>
-                <Text p={'3px'} fontSize={'14px'}>Time : Commit Time</Text>
-                <Text p={'3px'} fontSize={'14px'}>Author: Commit Author</Text>
+                <Text p={'3px'} fontSize={'14px'}>Time : {data.commit.author.date}</Text>
+                <Text p={'3px'} fontSize={'14px'}>Author: {data.commit.author.name}</Text>
             </Flex>
         </Box>
     )
